5f9661bf74ea0085cea46aeb23ce89acdcf2048d
Kelly Wilson <wilsonk@cpsc.ucalgary.ca>
2026-05-29 13:00:11 -0600

    Fix TargetRegistry.h include for LLVM 21
    - Change from llvm/Support/TargetRegistry.h to llvm/MC/TargetRegistry.h
    - LLVM 21 moved this header from Support to MC directory
    - Fixes compilation error with LLVM 21

--- Source/LLVM.cpp
+++ Source/LLVM.cpp
@@ -13,7 +13,7 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Constants.h>
 #include <llvm/Support/TargetSelect.h>
-#include <llvm/Support/TargetRegistry.h>
+#include <llvm/MC/TargetRegistry.h>
 #include <llvm/Target/TargetMachine.h>
 #include <llvm/IR/LegacyPassManager.h>
 #include <llvm/Support/FileSystem.h>
